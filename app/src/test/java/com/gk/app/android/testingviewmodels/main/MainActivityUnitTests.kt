06a71e4807b0322ec60e78a344d29247cbd5fc95
ghassen kefi <ghassen.kefi@gmail.com>
2020-10-06 10:03:59 +0200

    added missing test options & robolectric properties file

--- app/src/test/java/com/gk/app/android/testingviewmodels/main/MainActivityUnitTests.kt
+++ app/src/test/java/com/gk/app/android/testingviewmodels/main/MainActivityUnitTests.kt
@@ -7,12 +7,14 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
 import com.gk.app.android.testingviewmodels.R
 import com.gk.app.android.testingviewmodels.ui.main.MainActivity
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@SmallTest
 @RunWith(AndroidJUnit4::class)
 class MainActivityUnitTests {
 
