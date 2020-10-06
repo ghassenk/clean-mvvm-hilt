f4f99b0e9aab278e4d651df1a5cab6d960f95371
ghassen kefi <ghassen.kefi@gmail.com>
2020-10-06 17:25:46 +0200

    minor

--- app/src/main/java/com/gk/app/android/testingviewmodels/app/MyApp.kt
+++ app/src/main/java/com/gk/app/android/testingviewmodels/app/MyApp.kt
@@ -7,15 +7,9 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class MyApp : Application() {
 
-    companion object {
-        // Used for global activity callbacks
-        lateinit var appInstance: Application
-    }
-
     override fun onCreate() {
         Log.i(javaClass.simpleName, "onCreate()")
         super.onCreate()
-
-        appInstance = this
     }
+    
 }
